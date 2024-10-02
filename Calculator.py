2d1b0e1c0ba9fe2084d551600b1845487c175529
Ramela-M-S <ramlams07@gmail.com>
2024-10-02 08:26:32 +0530

    Created calculator python project

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+def arith(y):
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    if y==0:
+        print("Addition of a and b is:",a+b)
+    elif y==1:
+        print("Subraction of a and b is:",a-b)
+    elif y==2:
+        print("Multiplication  of a and b is:",a*b)
+    elif y==3:
+        print("Quotient of a and b is:",a/b)
+    else:
+        print("Remainder of a and b is:",a%b)
+
+
+
+
+while True:
+    print("Welcome to Calculator python project")
+    print("0 - For addition")
+    print("1 - For subraction")
+    print("2 - For multiplication")
+    print("3 - For quotient")
+    print("4 - For remainder")
+    x=int(input("enter the option:"))
+    if x>=0 and x<=4:
+        arith(x)
+    else:
+        print("Exiting the Calculator python project")
+        break
+
+    
